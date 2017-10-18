ad217bcad56a60c3cac7143634b90fb9a264766d
Ullrich Schäfer <ullrich@seidbereit.de>
2017-10-18 14:18:31 +0200

    Demonstrating post count after navigating back

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import Link from 'next/link'
+import withPosts from 'nextein/posts'
+import { Content } from 'nextein/post'
 
-const Index = () => (
+const Index = (props) => (
   <div>
     <Link href="/about">
       <a>About Page</a>
     </Link>
-    <p>Hello Next.js</p>
+    <p>Hello Nextin with {props.posts.length} Posts</p>
   </div>
 )
 
-export default Index
+export default withPosts(Index)
